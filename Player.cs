1dd35b72b33721bc4f32187b08374bb64fab3709
Naci Karakullukçu <n.karakullukcu@hotmail.com>
2021-02-07 02:22:13 +0300

    Add project files.

---
+++ Player.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+
+namespace ConstructorAndDestructorGame
+{
+    public class Player : IDisposable
+    {
+        public static int PlayerCount;
+        public string PlayerName { get; set; }
+        public Player()
+        {
+            PlayerCount++;
+            Console.WriteLine("A player joined the game :)");
+        }
+        static Player()
+        {
+            Player.PlayerCount = 0;
+        }
+
+        ~Player()
+        {
+            PlayerCount--;
+        }
+
+        public void Dispose()
+        {
+            Console.WriteLine("A player left the game :(");
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ConstructorAndDestructorGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (Player p1 = new Player())
+            {
+               
+            }
+        }
+    }
+}
